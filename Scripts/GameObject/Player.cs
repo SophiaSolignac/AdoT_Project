18f1d3f2f7db5e748009a2c65dcecb27329efbba
Dylan Lupon <dylan1lupon@gmail.com>
2024-05-30 15:15:51 +0200

    Add Player Movement
    You can move with the left stick and dash with the right stick

---
+++ Scripts/GameObject/Player.cs
@@ -0,0 +1,62 @@
+using Godot;
+using Com.AdoT_Project.Management;
+using System;
+using System.Runtime.CompilerServices;
+using static Godot.Control;
+
+namespace Com.AdoT_Project.GameObject
+{
+	public partial class Player : CharacterBody2D
+	{
+		// ----------------~~~~~~~~~~~~~~~~~~~==========================# // Variables
+
+		// MOVEMENTS
+		// Parametters
+		[Export] private float accelerationSpeed = 250f;
+		[Export] private float dashAcceleration = 1000f;
+        [Export] private float friction = .1f;
+		// Properties
+		private Action<float> Move;
+		private Vector2 acceleration = Vector2.Zero;
+		private Vector2 velocity = Vector2.Zero;
+		private Vector2 rotationVector = Vector2.Zero;
+
+		// ----------------~~~~~~~~~~~~~~~~~~~==========================# // Initialization
+		
+		public override void _Ready()
+		{
+			SetMoveInput();
+
+        }
+
+		// ----------------~~~~~~~~~~~~~~~~~~~==========================# // Do On Every Frame
+		
+		public override void _Process(double delta)
+		{
+			Move((float)delta);
+		}
+
+		// ----------------~~~~~~~~~~~~~~~~~~~==========================# // Movements
+
+		// Set Modes
+		private void SetMoveStatic() => Move = MoveStatic;
+
+		private void SetMoveInput() => Move = MoveInput;
+
+        // Movements
+        private void MoveStatic(float pDelta) { }
+
+		private void MoveInput(float pDelta)
+		{
+            acceleration = InputManager.Player.GetDirection() * accelerationSpeed;
+
+			if (velocity.Length() < 1) velocity = InputManager.Player.GetDashDirection() * dashAcceleration;
+
+            velocity *= Mathf.Pow(friction, pDelta);
+
+            Position += (acceleration + velocity) * pDelta;
+
+            if (InputManager.Player.GetDirection().Length() > 0) Rotation = Mathf.LerpAngle(Rotation, Mathf.Atan2(acceleration.Y, acceleration.X), .5f);
+        }
+	}
+}
---
+++ Scripts/Management/InputManager.cs
@@ -0,0 +1,59 @@
+using Godot;
+
+namespace Com.AdoT_Project.Management
+{
+	public static class InputManager
+	{
+		private const string NAME_PLAYER_GO_LEFT = "playerGoLeft";
+		private const string NAME_PLAYER_GO_UP = "playerGoUp";
+		private const string NAME_PLAYER_GO_RIGHT = "playerGoRight";
+		private const string NAME_PLAYER_GO_DOWN = "playerGoDown";
+
+        private const string NAME_PLAYER_DASH_LEFT = "playerDashLeft";
+        private const string NAME_PLAYER_DASH_UP = "playerDashUp";
+        private const string NAME_PLAYER_DASH_RIGHT = "playerDashRight";
+        private const string NAME_PLAYER_DASH_DOWN = "playerDashDown";
+
+        public static class Player
+		{
+			// JOY
+			private const float JOY_DETECTION = .2f;
+
+			// MOVEMENTS
+			private static float goLeftStrength => GetCorrectedStrength(NAME_PLAYER_GO_LEFT);
+			private static float goUpStrength => GetCorrectedStrength(NAME_PLAYER_GO_UP);
+			private static float goRightStrength => GetCorrectedStrength(NAME_PLAYER_GO_RIGHT);
+			private static float goDownStrength => GetCorrectedStrength(NAME_PLAYER_GO_DOWN);
+
+            private static float dashLeftStrength => GetCorrectedStrength(NAME_PLAYER_DASH_LEFT);
+            private static float dashUpStrength => GetCorrectedStrength(NAME_PLAYER_DASH_UP);
+            private static float dashRightStrength => GetCorrectedStrength(NAME_PLAYER_DASH_RIGHT);
+            private static float dashDownStrength => GetCorrectedStrength(NAME_PLAYER_DASH_DOWN);
+
+            private static Vector2 direction = Vector2.Zero;
+			private static Vector2 dashDirection = Vector2.Zero;
+
+			private static float GetCorrectedStrength(string pName) => Input.GetActionRawStrength(pName) < JOY_DETECTION ? 0 : Input.GetActionRawStrength(pName);
+
+            public static Vector2 GetDirection()
+			{
+				// Get Direction Based On Inputs
+				direction.X = goRightStrength - goLeftStrength;
+				direction.Y = goDownStrength - goUpStrength;
+				// Clamp Under 1f
+				if (direction.Length() > 1f) direction = direction.Normalized();
+
+                return direction;
+            }
+
+            public static Vector2 GetDashDirection()
+            {
+                // Get Direction Based On Inputs
+                dashDirection.X = dashRightStrength - dashLeftStrength;
+                dashDirection.Y = dashDownStrength - dashUpStrength;
+
+                return dashDirection.Normalized();
+            }
+        }
+	}
+}
